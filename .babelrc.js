9e42d3ace3523d5096c8d6c9253ac24a8bba1306
Alan Eicker <alaneicker@gmail.com>
2021-04-10 15:53:13 -0500

    Updates .eslinrc and .babelrc to handle @atomikui-core aliasing

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,17 @@
+const { plugins, ...config } = require('babel-config-atomikui');
+
 module.exports = {
-  ...require('babel-config-atomikui'),
+  ...config,
+  plugins: [
+    ...plugins,
+    [
+      'module-resolver',
+      {
+        root: './',
+        alias: {
+          '@atomikui-core': './node_modules/@atomikui/core/dist/components',
+        },
+      },
+    ],
+  ],
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from '@atomikui/core';
+import Link from '@atomikui-core/link';
 
 const App = () => (
   <div className="App">
