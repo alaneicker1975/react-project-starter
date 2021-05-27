4bb7caec2129039b45722c634f59d2c5eb9fd86e
Alan Eicker <ALANEICKER@GMAIL.COM>
2021-05-27 09:19:34 -0500

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -2,16 +2,5 @@ const { plugins, ...config } = require('babel-config-atomikui');
 
 module.exports = {
   ...config,
-  plugins: [
-    ...plugins,
-    [
-      'module-resolver',
-      {
-        root: './',
-        alias: {
-          '@atomikui-core': './node_modules/@atomikui/core/dist/components',
-        },
-      },
-    ],
-  ],
+  plugins: [...plugins],
 };
