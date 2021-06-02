7c85ebe4659f5d0610bfa78bb8e4785eafacf1cd
Alan Eicker <alaneicker@gmail.com>
2021-06-02 10:50:37 -0500

    Adds redux support for test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,34 @@
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import { shallow, configure } from 'enzyme';
 import App from './App';
 
 configure({ adapter: new Adapter() });
 
+const mockStore = configureMockStore([]);
+
 describe('<App />', () => {
   let app;
 
   beforeEach(() => {
-    app = shallow(<App />);
+    const store = mockStore({
+      title: 'Welcome!',
+      subtitle: 'Your project starts here.',
+      description:
+        'With support for Redux, Jest, Enzyme, ESLint, Stylelint, Prettier, React Axe, and Travis CI.',
+      links: [
+        { url: 'https://parceljs.org/', text: 'Parcel' },
+        { url: 'https://www.atomikui.com/', text: 'Atomik UI' },
+      ],
+    });
+
+    app = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
   });
 
   it('Should render without errors', () => {
